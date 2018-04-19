fd8ffb096595a289117e7caab87fc3d768bc6204
Khoa Huynh <khoawin92@gmail.com>
2018-04-19 01:10:32 -0700

    added POST and GET

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 
+
 // Application Setup
 const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 // Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
@@ -26,5 +28,23 @@ app.get('/api/v1/books', (req, res) => {
     .catch(console.error);
 });
 
-app.get('*', (req, res) => res.redirect(CLIENT_URL));
+app.get('/api/v1/books/:id', (req, res) => {
+  client.query(`SELECT * FROM books
+WHERE book_id=$1
+`, [req.params.id])
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+app.post('/api/v1/books', (req,res) => {
+  client.query(`INSERT INTO books
+  (title, author, isbn, image_url, description)
+  VALUES ($1, $2, $3, $4, $5);`,
+    [req.body.title,
+      req.body.author,
+      req.body.isbn,
+      req.body.image_url,
+      req.body.description]);
+});
+// app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
